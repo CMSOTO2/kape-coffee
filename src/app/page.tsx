f255d2dc014771f0d55171373d2d1d4506dd3df7
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-05-08 23:37:36 -0400

    integrate storyblok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import FeaturedProducts from './_components/FeaturedProducts';
+import Footer from './_components/Footer';
+import Hero from './_components/Hero';
+import Menu from './_components/Menu';
+import Navbar from './_components/Navbar';
+import WeAre from './_components/WeAre';
 
-export default function Home() {
+const getHomePageContent = async () => {
+  const version = process.env.SB_DATA_VERSION;
+  const token = process.env.SB_TOKEN;
+  const url = `https://api-us.storyblok.com/v2/cdn/stories/home-page?version=${version}&token=${token}`;
+
+  let req = await fetch(url, { cache: "no-store" });
+
+  const storyData = await req.json();
+  const { nav_section } = storyData.story.content;
+
+  return {
+    nav_section: nav_section[0]
+  }
+}
+
+export default async function Home() {
+  const storyData = await getHomePageContent();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+    <>
+      <Navbar content={storyData.nav_section} />
+      <Hero />
+      <FeaturedProducts />
+      <Menu />
+      <WeAre />
+      <Footer />
+    </>
   );
 }
