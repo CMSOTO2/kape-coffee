fd91e0e55f7a5fbc3547172ebd509372d5c067ab
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-05-28 21:10:53 -0400

    move stripe back to constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,9 @@
+import Stripe from "stripe";
+
 export const navLinks = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
   { label: "Contact", href: "/contact" },
 ] as const;
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
