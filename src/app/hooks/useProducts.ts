d321c379d54aa9b313f6312bd9cc3e4bb578b858
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-05-28 20:47:19 -0400

    add products hook and map 2

---
+++ src/app/hooks/useProducts.ts
@@ -0,0 +1,19 @@
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+
+interface ProductsType {
+  id: string;
+  active: boolean;
+  name: string;
+  description: string;
+  images: string[];
+  price: number;
+  currency: "usd";
+}
+
+export const useProducts = (): UseQueryResult<ProductsType[]> => {
+  return useQuery({
+    queryKey: ["product-list"],
+    queryFn: () => fetch("/api/products").then((res) => res.json()),
+    enabled: true,
+  });
+};
