7e7da66d25a83fa32334beb89ea4c64a43622b69
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-05-28 20:15:39 -0400

    add products from stripe call

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,8 +1,36 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
+interface ProductsType {
+  id: string;
+  active: boolean;
+  name: string;
+  description: string;
+  images: string[];
+  price: number;
+  currency: "usd";
+}
+
 export default function Hero() {
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["product-list"],
+    queryFn: () => fetch("/api/products"),
+    enabled: true,
+  });
+
+  if (isLoading) {
+    return "...loading";
+  }
+  if (isError) {
+    return "error";
+  }
   return (
     <div className="min-h-screen text-white">
+      {data?.data?.map((product: ProductsType) => {
+        return <div>{product.name}</div>;
+      })}
       <button>
         <a
           href="https://buy.stripe.com/test_4gwdR06529myd209AA"
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { stripe } from "../webhook/route";
+
+export const GET = async (req: Request, res: Response) => {
+  try {
+    const products = await stripe.products.list();
+    const prices = await stripe.prices.list({ active: true }); // Filter active prices
+
+    // Combine product and price information
+    const productsWithPrices = products.data.map((product) => {
+      const matchingPrice = prices.data.find(
+        (price) => price.product === product.id,
+      );
+      return {
+        ...product,
+        price: matchingPrice?.unit_amount, // Price if available, otherwise undefined
+        currency: matchingPrice?.currency, // Currency if available, otherwise undefined
+      };
+    });
+
+    return NextResponse.json(productsWithPrices);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Failed to retrieve products" });
+  }
+};
