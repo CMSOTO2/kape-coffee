5a908e1951dcd2023761c15f4ec4aa253ea3a591
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-06-01 15:06:54 -0400

    test checkout 2

--- src/app/_components/ShoppingCartProvider.tsx
+++ src/app/_components/ShoppingCartProvider.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from "react";
 import { CartProvider } from "use-shopping-cart";
 
 const ShoppingCartProvider = ({ children }: { children: ReactNode }) => {
-  const stripeKey = process.env.STRIPE_PUBLISHABLE_KEY as string;
+  const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string;
 
   return (
     <CartProvider
