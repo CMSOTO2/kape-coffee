36d69c77724c92632919978173079ba38fd6180a
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-06-15 13:43:41 -0400

    change hero size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="relative top-0 flex items-center justify-center">
-      <div className="z-10 flex h-[70vh] max-h-[700px] min-h-[500px] w-full items-center justify-center">
+      <div className="z-10 flex h-[90vh] w-full items-center justify-center">
         {/* TODO: wait for kape team to confirm text */}
         {/* <h1 className="absolute z-10 text-center text-7xl font-medium text-kape-beige">
           Modern Coffee.
